25c8f06c4bf6349101ed80171e990cd160d44f72
HenriqueMarques <hiq.marques@gmail.com>
2022-05-29 20:45:53 -0300

    ok

--- Estruturas_Condicionais/js/main.js
+++ Estruturas_Condicionais/js/main.js
@@ -57,3 +57,24 @@ else console.log('Não é um número!');
 console.clear();
 let resultado = 3 > 4 ? "Sim" : "Não";
 console.log(resultado)
+
+//Operador ternário - exemplo 2
+console.clear();
+let result = Math.PI < 4 ? console.log('Sim') : console.log('Não');
+
+// Switch
+const h = 11;
+const cor = h > 10 ? 'vermelha' : 'azul';
+console.clear();
+switch (cor) {
+    case 'vermelha' :
+    console.log('A cor é vermelha');
+    break;
+    case 'azul':
+        console.log('A cor é azul');
+        break;
+    default:
+        console.log('A cor não [e vermelha ou azul');
+        break;    
+
+}
